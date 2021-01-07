e87ab9f4d01cf2f905f183bcc07d447a3b5d344b
hateusername <296839688@qq.com>
2021-01-07 15:59:42 +0800

    Initial commit

---
+++ mr/worker.go
@@ -0,0 +1,82 @@
+package mr
+
+import (
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+)
+
+//
+// Map functions return a slice of KeyValue.
+//
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+//
+// use ihash(key) % NReduce to choose the reduce
+// task number for each KeyValue emitted by Map.
+//
+func ihash(key string) int {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return int(h.Sum32() & 0x7fffffff)
+}
+
+//
+// main/mrworker.go calls this function.
+//
+func Worker(mapf func(string, string) []KeyValue,
+	reducef func(string, []string) string) {
+
+	// Your worker implementation here.
+
+	// uncomment to send the Example RPC to the master.
+	CallExample()
+
+}
+
+//
+// example function to show how to make an RPC call to the master.
+//
+// the RPC argument and reply types are defined in rpc.go.
+//
+func CallExample() {
+
+	// declare an argument structure.
+	args := new(Assign) //实际上worker不需要给信息
+
+	// declare a reply structure.
+	reply := arg
+
+	// send the RPC request, wait for the reply.
+	call("Master.Apply", &args, &reply)
+
+	// reply.Y should be 100.
+	fmt.Println(reply.Info)
+}
+
+//
+// send an RPC request to the master, wait for the response.
+// usually returns true.
+// returns false if something goes wrong.
+//
+func call(rpcname string, args interface{}, reply interface{}) bool {
+	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	sockname := masterSock()
+	c, err := rpc.DialHTTP("unix", sockname)
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	defer c.Close()
+
+	err = c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+
+	fmt.Println(err)
+	return false
+}
